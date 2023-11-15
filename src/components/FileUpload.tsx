5129b20952fd79665967775d33021a5dcf2b1b50
qkhanh711 <quockhanh7112003@gmail.com>
2023-11-15 21:16:46 +0700

    bug

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { uploadToS3 } from '@/lib/s3'
-import { Inbox } from 'lucide-react'
+import { useMutation } from '@tanstack/react-query'
+import { Inbox, Loader2 } from 'lucide-react'
 import React from 'react'
 import { useDropzone } from 'react-dropzone'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import Loadable from 'next/dist/shared/lib/loadable.shared-runtime'
+
 
 const FileUpload = () => {
+  const [uploading, setUploading] = React.useState(false)
+  const {mutate, isPending} = useMutation({
+    mutationFn: async({file_key, file_name}: {file_key: string, file_name:string}) => {
+        const response = await axios.post('api/create-chat', {
+        file_key, 
+        file_name,
+      })
+      return response.data;
+    },
+  });
+
   const {getRootProps, getInputProps} = useDropzone({
     accept: {'application/pdf': ['.pdf']},
     maxFiles: 1,
@@ -12,33 +28,62 @@ const FileUpload = () => {
       console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10*1024*1024)  {
-        alert('File is too large. Please upload a file smaller than 10MB.')
-        return
+        toast.error('File is too large.Please upload a file smaller than 10MB.')
+        return;
       }
 
       try{
-        const data = await uploadToS3(file)
+        setUploading(true);
+        const data = await uploadToS3(file);
+        if (!data?.file_key || !data.file_name) {
+          toast.error('Someting went wrong!!. Error uploading file.')
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            // console.log('data', data)
+            toast.success(data.message )
+          },
+          onError: (error) => {
+             toast.error('Something went wrong!!. Error creatign chat.')
+           },
+        });
+        
         console.log('data',data)
       } catch (error){
         console.log(error)
+      } finally {
+        setUploading(false);
       }
 
     }, 
   })
   return (
-    <div className='p-2 bg-white rounded-x1'>
-      <div {...getRootProps({
-        className: 'border-dashed border-2 rounded-x1 cursor-pointer bg-grayy-50 py-8 flex justify-center items-center flex-col '
-      })}
+    <div className="p-2 bg-white rounded-xl">
+      <div
+        {...getRootProps({
+          className:
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
       >
         <input {...getInputProps()} />
-        <>
-        <Inbox className='w-10 h-10 text-blue-500'/>
-        <p className='mt-2 text-sm text-slate-400'>Drop PDF Here</p>
-        </>
+        {uploading || isPending ? (
+          <>
+            {/* loading state */}
+            <Loader2 className='h-10 w-10 text-blue-500 animate-spin' />
+            <p className='mt-2 text-sm text-slate-400'>
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FileUpload
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -8,8 +8,10 @@ export const chats = pgTable('chats', {
     pdfUrl:text('pdf_url').notNull(), 
     createdAt: timestamp('created_at').notNull().defaultNow(),
     userId: varchar('user_id', { length: 256 }).notNull(),
-    filekey: text('file_key').notNull(),  
+    fileKey: text('file_key').notNull(),  
 });
+
+export type DrizzleChat = typeof chats.$inferSelect;
 
 export const messages = pgTable('messages', {
     id: serial('id').primaryKey(),
@@ -17,6 +19,19 @@ export const messages = pgTable('messages', {
     content: text('content').notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     role: userSystemEnum('role').notNull(),
+});
+
+export const userSubscriptions = pgTable("user_subscriptions", {
+    id: serial("id").primaryKey(),
+    userId: varchar("user_id", { length: 256 }).notNull().unique(),
+    stripeCustomerId: varchar("stripe_customer_id", { length: 256 })
+      .notNull()
+      .unique(),
+    stripeSubscriptionId: varchar("stripe_subscription_id", {
+      length: 256,
+    }).unique(),
+    stripePriceId: varchar("stripe_price_id", { length: 256 }),
+    stripeCurrentPeriodEnd: timestamp("stripe_current_period_ended_at"),
 });
 
 //drizzle-orm
